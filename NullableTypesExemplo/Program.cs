d89bb5c1293dbdea7283ce505ad42ec1ebfb7568
Romario Angelo <romario-etical@hotmail.com>
2021-06-13 22:20:59 +0100

    Add project files.

---
+++ NullableTypesExemplo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NullableTypesExemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<DateTime> data = null;
+            //Console.WriteLine($"Data1: {data.Value}");
+            Console.WriteLine($"Data2: {data.GetValueOrDefault()}");
+            if (data.HasValue)
+            {
+                Console.WriteLine($"Data3: {data.Value}");
+            }
+            else
+            {
+                Console.WriteLine($"Data3: null");
+            }
+            //data = data == null ? DateTime.Now : data;
+            data = data ?? DateTime.Now;
+            Console.WriteLine($"Data4: {data}");
+        }
+    }
+}
